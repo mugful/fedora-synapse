abbdb7fffb47c48efe5484b5801a22b13e91159e
Jiří Stránský <jistr@jistr.com>
2018-12-19 21:12:11 +0100

    Fix the UID/GID to be under 65K

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 yum -y install jq matrix-synapse
 yum clean all
 
-groupmod -g 408448 synapse
-usermod -u 408448 synapse
+groupmod -g 48448 synapse
+usermod -u 48448 synapse
 chown -R synapse: /var/lib/synapse
